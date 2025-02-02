a3ad8d406c5fcfcc0ef596d4ee631daad00b4320
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-02 21:51:09 +0300

    feat:initialize firestore and router

--- firebaseapi/main.go
+++ firebaseapi/main.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 )
 
 const (
 	firebasConfigFile = "firebaseConfig.json"
-	firebaseDBURL = "https://your-firebase-project.firebaseio.com"
+	firebaseDBURL     = "https://your-firebase-project.firebaseio.com"
 )
 
 var (
@@ -17,18 +20,33 @@ var (
 	app *firebase.App
 )
 
+func main() {
 
-func main(){
+	ctx = context.Background()
 
-ctx = context.Background()
+	opt := option.WithCredentialsFile(firebasConfigFile)
 
-opt := option.WithCredentialsFile(firebasConfigFile)
+	app, err := firebase.NewApp(ctx, nil, opt)
 
-app, err := firebase.NewApp(ctx,, nil, opt)
+	if err != nil {
+		log.Fatalf("Firebase initialization error: %v\n", err)
+	}
 
-if err != nil {
-	log.Fatalf("Firebase initialization error: %v\n", err)
-}
+	client, err := app.DatabaseWithURL(ctx, firebaseDBURL)
 
+	if err != nil {
+		log.Fatalf("Firestore initialization error: %v\n", err)
+	}
+
+	router := mux.NewRouter()
+
+	// TO DO : Defining API routes
+
+
+
+	port := ":8080"
+
+	fmt.Printf("Server is running on port %s...\n", port)
+	log.Fatal(http.ListenAndServe(port, router))
 
 }
