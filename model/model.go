ad77182a506e592b84e82ebe6de93fb7f1733719
wyonyango <wycliffealphus7@gmail.com>
2025-02-10 15:45:36 +0300

    feat:add soft delete functionality

--- model/model.go
+++ model/model.go
@@ -7,9 +7,10 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `json:"id"`
-	Username  string    `json:"username"`
-	Email     string    `json:"email"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        uuid.UUID  `json:"id"`
+	Username  string     `json:"username"`
+	Email     string     `json:"email"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty"` // Soft delete field
 }
